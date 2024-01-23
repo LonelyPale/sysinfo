c946f156baafd36c3b53c2c602f979c83ffd44b4
LonelyPale <lonelypale@126.com>
2024-01-23 16:42:10 +0800

    update disk free

---
+++ examples/statfs-simple.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mount.h>
+
+int main() {
+    const char *path = "/";
+
+    struct statfs fs_info;
+
+    // 调用 statfs 函数
+    int result = statfs(path, &fs_info);
+
+    if (result == 0) {
+        // 获取块大小
+        printf("Block Size: %lu bytes\n", fs_info.f_bsize);
+
+        printf("f_type: %lu \n", fs_info.f_type);
+        printf("f_bsize: %lu \n", fs_info.f_bsize);
+        printf("f_blocks: %lu \n", fs_info.f_blocks);
+        printf("f_bfree: %lu \n", fs_info.f_bfree);
+        printf("f_bavail: %lu \n", fs_info.f_bavail);
+        printf("f_files: %lu \n", fs_info.f_files);
+        printf("f_ffree: %lu \n", fs_info.f_ffree);
+        printf("f_fsid: %lu \n", fs_info.f_fsid);
+        printf("f_flags: %lu \n", fs_info.f_flags);
+
+    } else {
+        // 错误处理
+        perror("Error calling statfs");
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+
+
+
---
+++ examples/statvfs-simple.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/statvfs.h>
+
+int main(int argc, char *argv[]) {
+    struct statvfs fs;
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <path>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    if (statvfs(argv[1], &fs) == -1) {
+        perror("statvfs");
+        exit(EXIT_FAILURE);
+    }
+    printf("Block size(f_bsize): %lu bytes\n", fs.f_bsize);
+    printf("Block size(f_frsize): %lu bytes\n", fs.f_frsize);
+    printf("Total blocks: %lu\n", fs.f_blocks);
+    printf("Free blocks: %lu\n", fs.f_bfree);
+    printf("Available blocks: %lu\n", fs.f_bavail);
+    printf("Inodes: %lu\n", fs.f_files);
+    printf("Free inodes: %lu\n", fs.f_ffree);
+    printf("Name max length: %lu\n", fs.f_namemax);
+    return 0;
+}
